9565d68578474af48c44888d296b66d5f7072b3c
Simone Margaritelli <evilsocket@gmail.com>
2021-01-05 12:00:40 +0100

    misc: small fix or general refactoring i did not bother commenting

--- host/utils.go
+++ host/utils.go
@@ -46,15 +46,15 @@ func hexToInt2(h string) (uint, uint) {
 func hexToIP(h string) net.IP {
 	n, m := hexToInt2(h)
 	var ip net.IP
+
 	if m != 0 {
-		ip = make(net.IP, 16)
-		// TODO: Check if this depends on machine endianness?
+		ip = make(net.IP, net.IPv6len)
 		binary.LittleEndian.PutUint32(ip, uint32(n>>32))
 		binary.LittleEndian.PutUint32(ip[4:], uint32(n))
 		binary.LittleEndian.PutUint32(ip[8:], uint32(m>>32))
 		binary.LittleEndian.PutUint32(ip[12:], uint32(m))
 	} else {
-		ip = make(net.IP, 4)
+		ip = make(net.IP, net.IPv4len)
 		binary.LittleEndian.PutUint32(ip, uint32(n))
 	}
 	return ip
